624abad51d3e734cf6b50fb8c1249325851bc932
perphyyoung <perphyyoung@hotmail.com>
2016-04-26 14:28:28 +0800

    update intro activity

--- src/main/java/edu/perphy/enger/IntroActivity.java
+++ src/main/java/edu/perphy/enger/IntroActivity.java
@@ -14,12 +14,14 @@ import edu.perphy.enger.fragment.IntroFragment;
 public class IntroActivity extends AppIntro {
     @Override
     public void init(@Nullable Bundle savedInstanceState) {
-        addSlide(IntroFragment.newInstance("Main", R.drawable.main));
+        addSlide(IntroFragment.newInstance("Main Layout", R.drawable.main));
         addSlide(IntroFragment.newInstance("Drawer Menu", R.drawable.drawer));
-        addSlide(IntroFragment.newInstance("Note", R.drawable.note));
+        addSlide(IntroFragment.newInstance("Manage Dictionaries", R.drawable.dict));
+        addSlide(IntroFragment.newInstance("Note List", R.drawable.note));
+        addSlide(IntroFragment.newInstance("Note Detail", R.drawable.note_detail));
         addSlide(IntroFragment.newInstance("Daily Sentence", R.drawable.daily));
-        addSlide(IntroFragment.newInstance("Star", R.drawable.star));
-        addSlide(IntroFragment.newInstance("Review", R.drawable.review));
+        addSlide(IntroFragment.newInstance("Star Lists", R.drawable.star));
+        addSlide(IntroFragment.newInstance("Review Layout", R.drawable.review));
     }
 
     @Override
